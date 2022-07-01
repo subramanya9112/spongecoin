1f359b9ac1d97f32fff30d49fe0dad75cbba4b29
renushreeg <63147175+renushreeg@users.noreply.github.com>
2022-07-01 19:43:48 +0530

    Main page
    Content in cards need to be changed

--- ui/src/components/Client/MainPage/index.js
+++ ui/src/components/Client/MainPage/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import Header from '../Header';
 import './index.scss';
+import mainPageBackgrndImg from "./../../../images/1.jpg";
+import mainPageContentBackgrndImg from "./../../../images/2.jpg";
 
 export default function Index() {
     return (
         <Header
             content={
-                <div>MainPage</div>
+                <div className="mainPage">
+                    <div className="mainPageHeading">
+                        <img src={mainPageBackgrndImg} alt="mainPageBackgrndImg" className="mainPageBackgrndImg" />
+                        <div className="mainPagecontainer">
+                            <h1>SPONGE COIN</h1>
+                        </div>
+                    </div>
+                    <div className="mainPagecontent">
+                        <img src={mainPageContentBackgrndImg} alt="mainPageContentBackgrndImg" className="mainPageContentBackgrndImg" />
+                        <div className="mainPageCard">
+                            <div className="mainPageCardRow">
+                                <div className="mainPageCards">
+                                    <div className="mainPageCardHeading">CREATE SIDE CHAIN</div>
+                                    <div className="mainPageCardContent">Create your own SIDE CHAIN in your name</div>
+                                </div>
+                                <div className="mainPageCards">
+                                    <div className="mainPageCardHeading">EASY TO INSTALL</div>
+                                    <div className="mainPageCardContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                                        molestiae quas vel sint repudiandae consequuntur voluptatum laborum</div>
+                                </div>
+                            </div>
+                            <div className="mainPageCardRow">
+                                <div className="mainPageCards">
+                                    <div className="mainPageCardHeading">LOW TRANSACTION TIME</div>
+                                    <div className="mainPageCardContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                                        molestiae quas vel sint repudiandae consequuntur voluptatum laborum</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             }
         />
     )
